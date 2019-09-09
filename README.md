39a61a59ce61cb72b3702b56071ff7d91ec8ac08
smojumd <santanu.mojumder@landsend.com>
2019-09-09 12:10:06 -0500

    for deploy

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lighthouse-batch-script
 
 script to generate lighthouse report
+
+./cli.js -c -h -v -d https://le-tst-2.landsend.com -f links.txt
--- index.js
+++ index.js
@@ -1,69 +1,101 @@
-'use strict'
+'use strict';
 require('shelljs/global');
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const OUT = './report/lighthouse'
-const REPORT_SUMMARY = 'summary.json'
-const JSON_EXT = '.report.json'
-const HTML_EXT = '.report.html'
-const CSV_EXT = '.report.csv'
+const OUT = './report/lighthouse';
+const REPORT_SUMMARY = 'summary.json';
+const JSON_EXT = '.report.json';
+const HTML_EXT = '.report.html';
+const CSV_EXT = '.report.csv';
 
-execute.OUT = OUT
+execute.OUT = OUT;
 module.exports = execute;
 
 function execute(options) {
-    log = log.bind(log, options.verbose || false)
+    log = log.bind(log, options.verbose || false);
 
-    const out = options.out || OUT
-    const lhScript = lighthouseScript(options, log)
-    const summaryPath = path.join(out, REPORT_SUMMARY)
+    const out = options.out || OUT;
+    const lhScript = lighthouseScript(options, log);
+    const summaryPath = path.join(out, REPORT_SUMMARY);
 
-    rm('-rf', out)
-    mkdir('-p', out)
+    rm('-rf', out);
+    mkdir('-p', out);
 
-    const count = options.sites.length
-    log(`Lighthouse batch run begin for ${count} site${count > 1 ? 's' : ''}`)
+    const count = options.sites.length;
+    log(`Lighthouse batch run begin for ${count} site${count > 1 ? 's' : ''}`);
 
     const reports = sitesInfo(options).map((site, i) => {
-        const prefix = `${i + 1}/${count}: `
-        const htmlOut = options.html ? ' --output html' : ''
-        console.log(options.csv)
-        const csvOut = options.csv ? ' --output csv' : ''
-        const filePath = path.join(out, site.file)
-        const customParams = options.params || ''
-        const chromeFlags = customParams.indexOf('--chrome-flags=') === -1 ? `--chrome-flags="--no-sandbox --headless --disable-gpu"` : ''
+        const prefix = `${i + 1}/${count}: `;
+        const htmlOut = options.html ? ' --output html' : '';
+        const csvOut = options.csv ? ' --output csv' : '';
+        const filePath = path.join(out, site.file);
+        const customParams = options.params || '';
+        const chromeFlags = customParams.indexOf('--chrome-flags=') === -1 ? `--chrome-flags="--no-sandbox --headless --disable-gpu"` : '';
         // if gen'ing html+json reports, ext '.report.json' is added by lighthouse cli automatically,
         // so here we try and keep the file names consistent by stripping to avoid duplication
-        const outputPath = options.html || options.csv ? filePath.slice(0, -JSON_EXT.length) : filePath
-        const cmd = `"${site.url}" --output json${htmlOut}${csvOut} --output-path "${outputPath}" ${chromeFlags} ${customParams}`
+        const outputPath = options.html || options.csv ? filePath.slice(0, -JSON_EXT.length) : filePath;
+        const cmd = `"${site.url}" --output json${htmlOut}${csvOut} --output-path "${outputPath}" ${chromeFlags} ${customParams}`;
 
-        log(`${prefix}Lighthouse analyzing '${site.url}'`)
-    log(cmd)
-    const outcome = exec(`${lhScript} ${cmd}`)
-    const summary = updateSummary(filePath, site, outcome, options)
+        log(`${prefix}Lighthouse analyzing '${site.url}'`);
+        log(cmd);
+        const outcome = exec(`${lhScript} ${cmd}`);
+        const summary = updateSummary(filePath, site, outcome, options);
+        postDataToSave(summary, filePath)
+        if (summary.error) console.warn(`${prefix}Lighthouse analysis FAILED for ${summary.url}`);
+        else log(`${prefix}Lighthouse analysis of '${summary.url}' complete with score ${summary.score}`);
 
-    if (summary.error) console.warn(`${prefix}Lighthouse analysis FAILED for ${summary.url}`)
-    else log(`${prefix}Lighthouse analysis of '${summary.url}' complete with score ${summary.score}`)
+        return summary
+    });
 
-    return summary
-})
-
-    log(`Lighthouse batch run end`)
-    log(`Writing reports summary to ${summaryPath}`)
+    log(`Lighthouse batch run end`);
+    log(`Writing reports summary to ${summaryPath}`);
     fs.writeFileSync(summaryPath, JSON.stringify(reports), 'utf8')
 }
 
+function postDataToSave(summary, filePath) {
+    const http = require('http')
+    const report = JSON.parse(fs.readFileSync(filePath))
+    log("FetchTime: " + report.fetchTime)
+    summary.runTime = report.fetchTime
+    const data = JSON.stringify(summary)
+    log("Data to be posted: " + data)
+    const options = {
+        hostname: 'localhost',
+        port: 8080,
+        path: '/metrics',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+
+    const req = http.request(options, (res) => {
+        log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', (d) => {
+            process.stdout.write(d)
+        })
+    })
+
+    req.on('error', (error) => {
+        console.error(error)
+    })
+    req.write(data)
+    req.end()
+}
+
 function sitesInfo(options) {
-    let sites = []
-    console.log(options.baseuri)
+    let sites = [];
+    console.log(options.baseuri);
     if (options.file) {
         try {
-            const contents = fs.readFileSync(options.file, 'utf8')
-            sites = contents.trim().split('\n').map(value => options.baseuri + value)
+            const contents = fs.readFileSync(options.file, 'utf8');
+            sites = contents.trim().split('\n').map(value => options.baseuri + value);
             console.log(sites)
         } catch (e) {
-            console.error(`Failed to read file ${options.file}, aborting.\n`, e)
+            console.error(`Failed to read file ${options.file}, aborting.\n`, e);
             process.exit(1)
         }
     }
@@ -71,42 +103,42 @@ function sitesInfo(options) {
         sites = sites.concat(options.sites)
     }
     return sites.map(url => {
-        url = url.trim()
+        url = url.trim();
         if (!url.match(/^https?:/)) {
-        if (!url.startsWith('//')) url = `//${url}`
-        url = `https:${url}`
-    }
-    const name = siteName(url)
-    const info = {
-        url,
-        name,
-        file: `${name}${JSON_EXT}`
-    }
-    if (options.html) info.html = `${name}${HTML_EXT}`
+            if (!url.startsWith('//')) url = `//${url}`;
+            url = `https:${url}`
+        }
+        const name = siteName(url);
+        const info = {
+            url,
+            name,
+            file: `${name}${JSON_EXT}`
+        };
+        if (options.html) info.html = `${name}${HTML_EXT}`;
+        return info;
+        if (options.html) info.csv = `${name}${CSV_EXT}`;
         return info
-    if (options.html) info.csv = `${name}${CSV_EXT}`
-        return info
-})
+    })
 }
 
 function lighthouseScript(options, log) {
     if (options.useGlobal) {
         if (exec('lighthouse --version').code === 0) {
-            log('Targeting global install of Lighthouse cli')
+            log('Targeting global install of Lighthouse cli');
             return 'lighthouse'
         } else {
             console.warn('Global Lighthouse install not found, falling back to local one')
         }
     }
-    let cliPath = path.resolve(`${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js`)
+    let cliPath = path.resolve(`${__dirname}/node_modules/lighthouse/lighthouse-cli/index.js`);
     if (!fs.existsSync(cliPath)) {
-        cliPath = path.resolve(`${__dirname}/../lighthouse/lighthouse-cli/index.js`)
+        cliPath = path.resolve(`${__dirname}/../lighthouse/lighthouse-cli/index.js`);
         if (!fs.existsSync(cliPath)) {
-            console.error(`Failed to find Lighthouse CLI, aborting.`)
+            console.error(`Failed to find Lighthouse CLI, aborting.`);
             process.exit(1)
         }
     }
-    log(`Targeting local Lighthouse cli at '${cliPath}'`)
+    log(`Targeting local Lighthouse cli at '${cliPath}'`);
     return `node ${cliPath}`
 }
 
@@ -116,28 +148,38 @@ function siteName(site) {
 
 function updateSummary(filePath, summary, outcome, options) {
     if (outcome.code !== 0) {
-        summary.score = 0
-        summary.error = outcome.stderr
+        summary.score = 0;
+        summary.error = outcome.stderr;
         return summary
     }
-    const report = JSON.parse(fs.readFileSync(filePath))
+    const report = JSON.parse(fs.readFileSync(filePath));
     return {
         ...summary,
-        ...getAverageScore(report)
+        ...getPerformanceMetrics(report)
+        // ...getAverageScore(report)
     }
 }
 
+function getPerformanceMetrics(report) {
+    let metricsItem = report.audits.metrics.details.items[0];
+    return metricsItem
+}
+
+function getPerformanceAutdits() {
+
+}
+
 function getAverageScore(report) {
-    let categories = report.reportCategories // lighthouse v1,2
+    let categories = report.reportCategories; // lighthouse v1,2
     if (report.categories) { // lighthouse v3
         categories = Object.values(report.categories)
     }
-    let total = 0
+    let total = 0;
     const detail = categories.reduce((acc, cat) => {
-        if (cat.id) acc[cat.id] = cat.score
-    total += cat.score
-    return acc
-}, {})
+        if (cat.id) acc[cat.id] = cat.score;
+        total += cat.score;
+        return acc
+    }, {});
     return {
         score: (total / categories.length).toFixed(2),
         detail
