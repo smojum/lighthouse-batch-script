8f3453d7b550185253eda63616acf9f29aaba302
Mojumder <Santanu.Mojumder@landsend.com>
2019-09-23 17:14:41 -0500

    startup script

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+while true;
+do 
+./cli.js -c -h -v -d https://le-dev-b.landsend.com -f links.txt; 
+./cli.js -c -h -v -d https://le-int-c.landsend.com -f links.txt; 
+./cli.js -c -h -v -d https://le-qas-a.landsend.com -f links.txt; 
+./cli.js -c -h -v -d https://www.landsend.com -f links.txt; 
+sleep 300;
+done
+
