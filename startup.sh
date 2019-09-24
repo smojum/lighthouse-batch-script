6e386754191e424f3d4d5abd52d6e13d37be3130
smojumd <santanu.mojumder@landsend.com>
2019-09-24 10:26:18 -0500

    upgraded lighthouse version + added for AWS dev-a instance.

--- startup.sh
+++ startup.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 while true;
 do 
 ./cli.js -c -h -v -d https://le-dev-b.landsend.com -f links.txt; 
 ./cli.js -c -h -v -d https://le-int-c.landsend.com -f links.txt; 
 ./cli.js -c -h -v -d https://le-qas-a.landsend.com -f links.txt; 
-./cli.js -c -h -v -d https://www.landsend.com -f links.txt; 
-sleep 600;
+./cli.js -c -h -v -d https://www.landsend.com -f links.txt;
+./cli.js -c -h -v -p "--chrome-flags=\"--headless --ignore-certificate-errors\"" -d https://le-deva-aws.landsend.com -f links.txt;
+sleep 900;
 done
 
