6b2b15502bd8008334222c8368d13038f5b056bc
smojumd <santanu.mojumder@landsend.com>
2019-09-25 10:00:23 -0500

    Added origin m1 to verify against production.

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ do
 ./cli.js -c -h -v -d https://le-qas-a.landsend.com -f links.txt; 
 ./cli.js -c -h -v -d https://www.landsend.com -f links.txt;
 ./cli.js -c -h -v -p "--chrome-flags=\"--headless --ignore-certificate-errors\"" -d https://le-deva-aws.landsend.com -f links.txt;
+./cli.js -c -h -v -d https://origin-m1-www.landsend.com -f links.txt;
 sleep 900;
 done
 
